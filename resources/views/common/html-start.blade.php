f9d2b95b4bb96177ca716c0ec5fae753a3a7c6a5
PragueBeerGuide <vondra.ctu@gmail.com>
2023-02-24 15:49:23 +0100

    update html

--- resources/views/common/html-start.blade.php
+++ resources/views/common/html-start.blade.php
@@ -7,29 +7,32 @@
     <title>St. Hector's Veterinary Clinic</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
-<header>
-    <!--NAV BAR-->
-    <nav class="navbar navbar-expand-sm bg-body-secondary px-3">
-        
-        <div class="container-fluid">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled">About</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled">Contact</a>
-              </li>
-            </ul>
-          </div>
 
-          <form class="d-flex w-50" role="search" action="/search" method="GET">
-            <input class="form-control me-3" type="search" name="q" placeholder="Search..." aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
-
-    </nav>
-  </header>
 <body>
+    <header>
+        <!--NAV BAR-->
+        <nav class="navbar fixed-top navbar-expand-sm bg-body-secondary px-3">
+            
+            <div class="container-fluid">
+                <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled">Contact</a>
+                </li>
+                </ul>
+            </div>
+
+            <form class="d-flex w-50" role="search" action="/search" method="GET">
+                <input class="form-control me-3" type="search" name="q" placeholder="Search..." aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+
+        </nav>
+  </header>
+
+  <main class="px-3">
